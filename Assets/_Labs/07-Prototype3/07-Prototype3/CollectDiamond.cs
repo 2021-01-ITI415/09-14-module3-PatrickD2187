5bab9f82a099e00dbdffb23b39077ffdae2096f9
PatrickD2187 <60270720+PatrickD2187@users.noreply.github.com>
2021-05-05 20:04:54 -0400

    Prototype3

---
+++ Assets/_Labs/07-Prototype3/07-Prototype3/CollectDiamond.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectDiamond : MonoBehaviour
+{
+    public AudioSource collectSound;
+
+    void Update()
+    {
+        transform.Rotate(new Vector3(15, 30, 45) * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        collectSound.Play();
+        ScoringSystem.theScore += 50;
+        Destroy(gameObject);
+    }
+}
---
+++ Assets/_Labs/07-Prototype3/07-Prototype3/WaterAudio.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+
+public class WaterAudio : MonoBehaviour
+{
+    public AudioClip splashSound;
+    public AudioClip Ambiance;
+
+    public AudioSource audioS;
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+        if (other.CompareTag("Ambience"))
+        {
+            audioS.PlayOneShot(Ambiance);
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Water"))
+        {
+            audioS.PlayOneShot(splashSound);
+        }
+        
+    }
+}
