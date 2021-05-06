99e2b42b32381cd56b4baae04575f6f50178deaa
PatrickD2187 <60270720+PatrickD2187@users.noreply.github.com>
2021-05-06 11:37:58 -0400

    UI Elements

--- Assets/_Labs/07-Prototype3/07-Prototype3/VictoryScreenEnd.cs
+++ Assets/_Labs/07-Prototype3/07-Prototype3/VictoryScreenEnd.cs
@@ -1,37 +1,28 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.UI;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
-public class ScoringSystem : MonoBehaviour
+public class VictoryScreenEnd : MonoBehaviour
 {
-    public GameObject scoreText;
     public GameObject VictoryText;
-    public static int theScore;
-
-
     void Start()
     {
         VictoryText.SetActive(false);
-
     }
 
-    void Update()
+    void OnTriggerEnter (Collider player) 
     {
-        scoreText.GetComponent<Text>().text = "SCORE: " + theScore;
-
-        if (theScore == 3000)
+        if(player.gameObject.tag == "Player")
         {
             VictoryText.SetActive(true);
             StartCoroutine("WaitForSec");
         }
-
-    }     
-        
+    }
     IEnumerator WaitForSec()
     {
-         yield return new WaitForSeconds(3);
-         SceneManager.LoadScene("07-Prototype3");
+        yield return new WaitForSeconds(3);
+        SceneManager.LoadScene("07-Prototype3");
     }
 }
