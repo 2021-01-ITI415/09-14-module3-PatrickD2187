2664e3b27aac387e48d5603d92e3e8ffbb92bd4c
PatrickD2187 <60270720+PatrickD2187@users.noreply.github.com>
2021-05-05 21:22:48 -0400

    Prototype3

--- Assets/_Labs/07-Prototype3/07-Prototype3/WaterAudio.cs
+++ Assets/_Labs/07-Prototype3/07-Prototype3/WaterAudio.cs
@@ -7,8 +7,15 @@ public class WaterAudio : MonoBehaviour
 {
     public AudioClip splashSound;
     public AudioClip Ambiance;
+    public AudioClip VictorySound;
+    public AudioClip Track2_Aux;
 
     public AudioSource audioS;
+
+    private void Update()
+    {
+
+    }
 
     private void OnTriggerEnter(Collider other)
     {
@@ -20,6 +27,14 @@ public class WaterAudio : MonoBehaviour
         {
             audioS.PlayOneShot(Ambiance);
         }
+        if (other.CompareTag("Finish"))
+        {
+            audioS.PlayOneShot(VictorySound);
+        }
+        if (other.CompareTag("Enemy"))
+        {
+            audioS.PlayOneShot(Track2_Aux);
+        };
     }
 
     private void OnTriggerExit(Collider other)
